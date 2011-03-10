8623833b71ebcfcc56128a0d3b4c59e4d5f81567
jeremypublictext <jeremy@publictext.net>
2011-03-10 10:26:55 +0000

    Working with Cabana now.

--- startup.js
+++ startup.js
@@ -1,6 +1,7 @@
-var http = require("http");
-var url = require("url");
-var querystring = require("querystring");
+var node = { };
+node.http = require("http");
+node.url = require("url");
+node.querystring = require("querystring");
 
 var ip = process.argv[2];
 var port = process.argv[3];
@@ -9,41 +10,45 @@ var rooms = { };
 var getCommands = { };
 var postCommands = { };
 
-getCommands["/v1/messages.jsonp"] = function(params, request, response, callback) {
-	if( !params.hasOwnProperty("callback") ) { 
+getCommands["/v1/messages.json"] = getCommands["/v1/messages.jsonp"] = function(url, request, response, callback) {
+	if( url.pathname.indexOf("jsonp") !== -1 && !url.query.hasOwnProperty("callback") ) { 
 		callback("expected param callback.");
 	}
-	if( !params.hasOwnProperty("room") ) {
+	if( !url.query.hasOwnProperty("room") ) {
 		callback("expected param room.");
 	}
 	var answer = { messages: [ ] };
 	response.writeHead(200, {"Content-Type": "application/json"});
-	if( rooms.hasOwnProperty(params.room) ) {
+	if( rooms.hasOwnProperty(url.query.room) ) {
 		var since;
-		if( params.hasOwnProperty("since") ) {
-			since = Number(params.since);
+		if( url.query.hasOwnProperty("since") ) {
+			since = Number(url.query.since);
 		} else {
 			since = (new Date()).getTime() - 24 * 60 * 60 * 2;
 		}
-		var messages = rooms[params.room];
+		var messages = rooms[url.query.room];
 		for( var i = 0; i < messages.length; i++ ) { 
 			var message = messages[i];
-			if( message.timestamp > since ) {
+		//	if( message.timestamp > since ) {
 				answer.messages.push(message);
-			}	
+		//	}	
 		}
 	}
-	response.end(params.callback + "(" + JSON.stringify(answer) + ");");
+	if( url.pathname.indexOf("jsonp") !== -1 ) {
+		response.end(url.query.callback + "(" + JSON.stringify(answer) + ");");
+	} else {
+		response.end(JSON.stringify(answer));
+	}
 	callback(undefined);
 };
 
-getCommands["/robots.txt"] = function(params, request, response, callback) {
+getCommands["/robots.txt"] = function(url, request, response, callback) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.end("User-agent: *\nDisallow: /\n");
 	callback(undefined);
 };
 
-postCommands["/v1/message.jsonp"] = function(params, request, response, callback) {
+postCommands["/v1/messages.json"] = function(url, request, response, callback) {
 	var payload = "";
 	request.addListener("data", function(chunk) {
 		payload = payload + chunk;
@@ -75,19 +80,19 @@ postCommands["/v1/message.jsonp"] = function(params, request, response, callback
 	});
 };
 
-var httpServer = http.createServer(function(request, response) {
+var httpServer = node.http.createServer(function(request, response) {
 	request.connection.removeAllListeners("error");
 	request.connection.on("error", function(error) {
 	});
-	var parsedUrl = url.parse(request.url, true);
-	if( typeof(parsedUrl.pathname) !== "string" ) {
-		parsedUrl.pathname = "/";
+	var url = node.url.parse(request.url, true);
+	if( typeof(url.pathname) !== "string" ) {
+		url.pathname = "/";
 	}
-	var endpoint = querystring.unescape(parsedUrl.pathname)
+	var endpoint = node.querystring.unescape(url.pathname)
 	var authentication = request.headers["authentication"];
 	if( request.method === "GET" ) {
 		if( getCommands.hasOwnProperty(endpoint) ) {
-			getCommands[endpoint](parsedUrl.query, request, response, function(error) {
+			getCommands[endpoint](url, request, response, function(error) {
 				if( error ) {
 					response.writeHead(500, {"Content-Type" : "application/javascript"});
 					response.end(JSON.stringify({ error: error}));
@@ -99,7 +104,7 @@ var httpServer = http.createServer(function(request, response) {
 		}
 	} else if( request.method === "POST" ) {
                 if( postCommands.hasOwnProperty(endpoint) ) {
-                        postCommands[endpoint](parsedUrl.query, request, response, function(error) {
+                        postCommands[endpoint](url, request, response, function(error) {
                                 if( error ) {
                                         response.writeHead(500, {"Content-Type" : "application/javascript"});
 					response.end(JSON.stringify({error: error}));
